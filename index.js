cdba590fc24dd30c934e50534223c214fd0b492f
DANIIL GOLOTVIN <daniilgolotvin@yandex.ru>
2024-03-28 13:42:30 +0300

    AddIconCamera And ToggleIconTheme

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
+const cameraToggleBtn = document.getElementById('cameraToggleBtn');
 const videoElement = document.getElementById('videoElement');
-const startButton = document.getElementById('startButton');
-const stopButton = document.getElementById('stopButton');
+const icon = document.getElementById('icon');
+const themeToggleBtn = document.getElementById('themeToggleBtn');
 let stream;
 
-// Функция для запуска видео с веб-камеры
+cameraToggleBtn.addEventListener('click', () => {
+  if (!stream) {
+    startVideo();
+  } else {
+    stopVideo();
+  }
+});
+
 async function startVideo() {
   try {
     stream = await navigator.mediaDevices.getUserMedia({ video: true });
     videoElement.srcObject = stream;
+    icon.style.backgroundImage = "url('./image/camera-video-svgrepo-com.svg')";
+    cameraToggleBtn.classList.add('active');
   } catch (err) {
     console.error('Ошибка при получении доступа к веб-камере:', err);
   }
 }
 
-// Функция для отключения видео
 function stopVideo() {
   if (stream) {
     const tracks = stream.getTracks();
     tracks.forEach(track => track.stop());
     videoElement.srcObject = null;
+    stream = null;
+    icon.style.backgroundImage = "url('./image/camera-video-off-svgrepo-com.svg')";
+    cameraToggleBtn.classList.remove('active');
   }
 }
 
-startButton.addEventListener('click', startVideo);
-stopButton.addEventListener('click', stopVideo);
+themeToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+  themeToggleBtn.classList.toggle('container__table-buttontoggle_dark');
+});
